b1e5937aba937ae96ca74e0e3be63aa13e866adf
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:31:07 -0400

    Add files via upload

---
+++ 18-Evaluacion_temperatura/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Ejercicio18
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la temperatura en grados Celsius: ");
+        double temperatura = Convert.ToDouble(Console.ReadLine());
+
+        if (temperatura < 0)
+        {
+            Console.WriteLine("\n'Hace mucho frío'");
+        }
+        else if (temperatura >= 0 && temperatura <= 20)
+        {
+            Console.WriteLine("\n'Clima fresco'");
+        }
+        else if (temperatura >= 21 && temperatura <= 30)
+        {
+            Console.WriteLine("\n'Clima agradable'");
+        }
+        else
+        {
+            Console.WriteLine("\n'Hace mucho calor'");
+        }
+    }
+}
