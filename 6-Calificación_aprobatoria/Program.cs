6cc0343bb34d2396371126bb67a793821e8f88cf
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:20:23 -0400

    Add files via upload

---
+++ 6-Calificación_aprobatoria/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Ejercicio06
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la calificacion (0-100): ");
+        double calificacion = Convert.ToDouble(Console.ReadLine());
+
+        if (calificacion >= 60)
+        {
+            Console.WriteLine("El estudiante aprobo.");
+        }
+        else
+        {
+            Console.WriteLine("El estudiante reprobo.");
+        }
+    }
+}
