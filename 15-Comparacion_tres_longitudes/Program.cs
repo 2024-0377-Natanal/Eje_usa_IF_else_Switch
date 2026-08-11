06abc7b0197248d5bf6367b62c1b7bac2328cc63
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:28:50 -0400

    Add files via upload

---
+++ 15-Comparacion_tres_longitudes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Ejercicio15
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la primera longitud: ");
+        double lado1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese la segunda longitud: ");
+        double lado2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese la tercera longitud: ");
+        double lado3 = Convert.ToDouble(Console.ReadLine());
+
+        if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1)
+        {
+            Console.WriteLine("Las longitudes SI pueden formar un triangulo.");
+        }
+        else
+        {
+            Console.WriteLine("Las longitudes NO pueden formar un triangulo.");
+        }
+    }
+}
