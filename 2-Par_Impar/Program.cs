150dc79988c2044576d57f2018d079cfe2fe2ad8
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:09:59 -0400

    Add files via upload

---
+++ 2-Par_Impar/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio02
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un numero entero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine("El numero es par.");
+        }
+        else
+        {
+            Console.WriteLine("El numero es impar.");
+        }
+    }
+}
