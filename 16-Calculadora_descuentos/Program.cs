67cfc34efc15444b5b934a4e69d700af433c99fe
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:29:36 -0400

    Add files via upload

---
+++ 16-Calculadora_descuentos/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Ejercicio16
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el precio del producto: ");
+        double precio = Convert.ToDouble(Console.ReadLine());
+
+        double porcentajeDescuento;
+
+        if (precio < 50)
+        {
+            porcentajeDescuento = 0;
+        }
+        else if (precio >= 50 && precio <= 100)
+        {
+            porcentajeDescuento = 0.05;
+        }
+        else
+        {
+            porcentajeDescuento = 0.10;
+        }
+
+        double descuento = precio * porcentajeDescuento;
+        double precioFinal = precio - descuento;
+
+        Console.WriteLine("Descuento aplicado: " + (porcentajeDescuento * 100) + "%");
+        Console.WriteLine("Monto del descuento: " + descuento);
+        Console.WriteLine("Precio final: " + precioFinal);
+    }
+}
