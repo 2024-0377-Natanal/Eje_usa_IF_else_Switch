94af699f22abaf6c08f733cd15fe70146d53c4cb
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 20:53:38 -0400

    Add files via upload

---
+++ 10-Clasificacion_angulos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Ejercicio10
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el valor del angulo en grados: ");
+        double angulo = Convert.ToDouble(Console.ReadLine());
+
+        if (angulo < 90)
+        {
+            Console.WriteLine("El angulo es agudo.");
+        }
+        else if (angulo == 90)
+        {
+            Console.WriteLine("El angulo es recto.");
+        }
+        else if (angulo > 90 && angulo < 180)
+        {
+            Console.WriteLine("El angulo es obtuso.");
+        }
+        else if (angulo == 180)
+        {
+            Console.WriteLine("El angulo es llano.");
+        }
+        else
+        {
+            Console.WriteLine("El valor del angulo esta fuera de rango (0-180).");
+        }
+    }
+}
---
+++ 9-Mayor_tres_numeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Ejercicio09
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer numero: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo numero: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer numero: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        double mayor;
+
+        if (num1 >= num2 && num1 >= num3)
+        {
+            mayor = num1;
+        }
+        else if (num2 >= num1 && num2 >= num3)
+        {
+            mayor = num2;
+        }
+        else
+        {
+            mayor = num3;
+        }
+
+        Console.WriteLine("El mayor de los tres numeros es: " + mayor);
+    }
+}
