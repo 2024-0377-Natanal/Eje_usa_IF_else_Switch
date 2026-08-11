88c0df7830bdb2019ae2dabd15170d4e7b817baf
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:21:35 -0400

    Add files via upload

---
+++ 7-Dia_semana/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Ejercicio07
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un numero del 1 al 7: ");
+        int dia = Convert.ToInt32(Console.ReadLine());
+
+        switch (dia)
+        {
+            case 1:
+                Console.WriteLine("\n'Lunes'");
+                break;
+            case 2:
+                Console.WriteLine("\n'Martes'");
+                break;
+            case 3:
+                Console.WriteLine("\n'Miercoles'");
+                break;
+            case 4:
+                Console.WriteLine("\n'Jueves'");
+                break;
+            case 5:
+                Console.WriteLine("\n'Viernes'");
+                break;
+            case 6:
+                Console.WriteLine("\n'Sabado'");
+                break;
+            case 7:
+                Console.WriteLine("\n'Domingo'");
+                break;
+            default:
+                Console.WriteLine("Numero invalido, debe estar entre 1 y 7.");
+                break;
+        }
+    }
+}
