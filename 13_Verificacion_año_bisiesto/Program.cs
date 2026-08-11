861d1f83e8eda6ca3b2cddbd03012fc17cb05e9f
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:27:24 -0400

    Add files via upload

---
+++ 13_Verificacion_año_bisiesto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio13
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el año: ");
+        int anio = Convert.ToInt32(Console.ReadLine());
+
+        if ((anio % 4 == 0 && anio % 100 != 0) || anio % 400 == 0)
+        {
+            Console.WriteLine("El año " + anio + " es bisiesto.");
+        }
+        else
+        {
+            Console.WriteLine("El año " + anio + " no es bisiesto.");
+        }
+    }
+}
