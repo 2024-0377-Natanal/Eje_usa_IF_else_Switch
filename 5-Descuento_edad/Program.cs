55a00553d166d2d1a666f205f25f85d2b2852aba
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:18:18 -0400

    Add files via upload

---
+++ 5-Descuento_edad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio05
+{
+    static void Main()
+    {
+        Console.Write("Ingresa tu edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad > 60)
+        {
+            Console.WriteLine("Aplicas para el descuento del 50%.");
+        }
+        else
+        {
+            Console.WriteLine("No aplicas para el descuento.");
+        }
+    }
+}
