c9c31691de29d1f8e08647db4bf832a555ca0a9c
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:28:11 -0400

    Add files via upload

---
+++ 14-Conversion_calificaciones/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Ejercicio14
+{
+    static void Main()
+    {
+        Console.Write("Ingrese la calificacion numerica (0-100): ");
+        int calificacion = Convert.ToInt32(Console.ReadLine());
+
+        char letra;
+
+        switch (calificacion / 10)
+        {
+            case 10:
+            case 9:
+                letra = 'A';
+                break;
+            case 8:
+                letra = 'B';
+                break;
+            case 7:
+                letra = 'C';
+                break;
+            case 6:
+                letra = 'D';
+                break;
+            default:
+                letra = 'F';
+                break;
+        }
+
+        Console.WriteLine("La calificacion en letra es: " + letra);
+    }
+}
