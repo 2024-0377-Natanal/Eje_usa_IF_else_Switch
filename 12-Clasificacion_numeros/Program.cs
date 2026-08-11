3be30337af26c2f5ebfa700a9f8e638c9dd7d7f7
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:26:41 -0400

    Add files via upload

---
+++ 12-Clasificacion_numeros/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Ejercicio12
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer numero: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo numero: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el tercer numero: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        if (num1 == 0 || num2 == 0 || num3 == 0)
+        {
+            Console.WriteLine("Hay al menos un cero entre los numeros.");
+        }
+        else if (num1 > 0 && num2 > 0 && num3 > 0)
+        {
+            Console.WriteLine("Todos los numeros son positivos.");
+        }
+        else if (num1 < 0 && num2 < 0 && num3 < 0)
+        {
+            Console.WriteLine("Todos los numeros son negativos.");
+        }
+        else
+        {
+            Console.WriteLine("Los numeros son mixtos.");
+        }
+    }
+}
