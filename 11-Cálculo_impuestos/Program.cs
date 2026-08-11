f5e4b478b3b4014a56544da9d780a70a6bdc7723
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:24:31 -0400

    Add files via upload

---
+++ 11-Cálculo_impuestos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Ejercicio11
+{
+    static void Main()
+    {
+        Console.Write("Ingresa tu salario mensual: ");
+        double salario = Convert.ToDouble(Console.ReadLine());
+
+        double impuesto;
+        double porcentaje;
+
+        if (salario < 10000)
+        {
+            porcentaje = 0;
+        }
+        else if (salario >= 10000 && salario <= 30000)
+        {
+            porcentaje = 0.10;
+        }
+        else
+        {
+            porcentaje = 0.20;
+        }
+
+        impuesto = salario * porcentaje;
+        double salarioNeto = salario - impuesto;
+
+        Console.WriteLine("Porcentaje de impuesto: " + (porcentaje * 100) + "%");
+        Console.WriteLine("Monto del impuesto: " + impuesto);
+        Console.WriteLine("Salario neto: " + salarioNeto);
+    }
+}
