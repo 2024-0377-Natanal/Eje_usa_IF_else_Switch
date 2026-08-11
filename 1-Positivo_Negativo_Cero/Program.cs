6de67b783feb2ce742a579dbc0b492fda6a47215
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:07:31 -0400

    Add files via upload

---
+++ 1-Positivo_Negativo_Cero/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Ejercicio01
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un numero: ");
+        double numero = Convert.ToDouble(Console.ReadLine());
+
+        if (numero > 0)
+        {
+            Console.WriteLine("El numero es positivo.");
+        }
+        else if (numero < 0)
+        {
+            Console.WriteLine("El numero es negativo.");
+        }
+        else
+        {
+            Console.WriteLine("El numero es cero.");
+        }
+    }
+}
