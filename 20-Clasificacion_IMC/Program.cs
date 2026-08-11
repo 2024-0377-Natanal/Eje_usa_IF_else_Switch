7189129d8bfbb279d028e573d06f5c48400925f2
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:32:54 -0400

    Add files via upload

---
+++ 20-Clasificacion_IMC/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Ejercicio20
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su peso en (kg): ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese su altura en (m): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double imc = peso / (altura * altura);
+
+        string clasificacion;
+
+        if (imc < 18.5)
+        {
+            clasificacion = "Bajo peso";
+        }
+        else if (imc >= 18.5 && imc <= 24.9)
+        {
+            clasificacion = "Normal";
+        }
+        else if (imc >= 25 && imc <= 29.9)
+        {
+            clasificacion = "Sobrepeso";
+        }
+        else
+        {
+            clasificacion = "Obesidad";
+        }
+
+        Console.WriteLine("Tu IMC es: " + Math.Round(imc, 2));
+        Console.WriteLine("Clasificación: " + clasificacion);
+    }
+}
