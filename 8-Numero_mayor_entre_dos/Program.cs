8bbdc7f654140b31193a1acd4c3a246e52b30e44
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:22:47 -0400

    Add files via upload

---
+++ 8-Numero_mayor_entre_dos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Ejercicio08
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el primer numero: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese el segundo numero: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        if (num1 > num2)
+        {
+            Console.WriteLine("El mayor es: " + num1);
+        }
+        else if (num2 > num1)
+        {
+            Console.WriteLine("El mayor es: " + num2);
+        }
+        else
+        {
+            Console.WriteLine("Ambos numeros son iguales.");
+        }
+    }
+}
