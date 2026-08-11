69cfff9cce4a3aa380148602624e26dd96cba1e0
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:11:41 -0400

    Add files via upload

---
+++ 3-Mayor_edad/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio03
+{
+    static void Main()
+    {
+        Console.Write("Ingrese su edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+
+        if (edad >= 18)
+        {
+            Console.WriteLine("Eres mayor de edad.");
+        }
+        else
+        {
+            Console.WriteLine("Eres menor de edad.");
+        }
+    }
+}
