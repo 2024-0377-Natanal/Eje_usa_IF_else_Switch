622f94b023c4d35b8c9020284a42daf1838c5084
Natanael Mejía <2024-0377@unad.edu.do>
2026-08-10 21:16:53 -0400

    Add files via upload

---
+++ 4-Multiplo_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Ejercicio04
+{
+    static void Main()
+    {
+        Console.Write("Ingrese un numero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 5 == 0)
+        {
+            Console.WriteLine("El numero es un multiplo de 5.");
+        }
+        else
+        {
+            Console.WriteLine("El numero NO es un multiplo de 5.");
+        }
+    }
+}
